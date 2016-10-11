157d114d0b1df893af3e04436bf2ca96282de342
Jakub Pola <jakub.pola@gmail.com>
2016-10-11 12:11:06 +0200

    New fantastic functionality - completed

--- main.cpp
+++ main.cpp
@@ -6,11 +6,17 @@ void PrintMessage()
     std::cout << "Ala ma kota a kot ma Alę" << std::endl;
 }
 
+void NewFunctionallityFunction()
+{
+    std::cout << "Fantastic new functionallity !" << std::endl;
+}
+
 int main()
 {
     std::cout << "Hello World!" << std::endl;
 
     PrintMessage();
+    NewFunctionallityFunction();
     return 0;
 }
 
